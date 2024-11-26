be9571be815a8099b727da72f43e3f5676325b1d
Savannah Pacheco <139471337+skathrinyoung@users.noreply.github.com>
2024-11-26 14:35:27 -0500

    Update Temperature Convertion Code.py

--- Temperature Convertion Code.py
+++ Temperature Convertion Code.py
@@ -1,10 +1,9 @@
-print("Hello, I hope you are having a wonderful today. Welcome to the temperature convertor. Please type if you would like to convert from 'Fahrenheit' or 'Celsius'.") ##Initial client-facing line
-tempMeasurement = input() ##Client's input into if they will be presenting data as fahrenheit or as celsius 
+
+tempMeasurement = input("Hello, I hope you are having a wonderful today. Welcome to the temperature convertor. Please type if you would like to convert from 'Fahrenheit' or 'Celsius'? ") ##Client's input into if they will be presenting data as fahrenheit or as celsius 
 
 ##Code to switch from Fahrenheit to Celsius
 if (tempMeasurement == "Fahrenheit"): ## If user inputs fahrenheit 
-    print("Great! What is the temperature you would like to convert?") ## Gets users to input the number they are looking to convert
-    tempFahrenheit = input() ## Collects user's answer in the tempFahrenheit variable
+    tempFahrenheit = input("Great! What is the temperature you would like to convert? ") ## Collects user's answer in the tempFahrenheit variable
     print("Wonderful, let me do some quick math to convert " + tempFahrenheit + "F to Celsius for you.") ## lets the user know what the computer is doing, acts as a confirmation that data was input correctly
     tempFahrenheit = float(tempFahrenheit) ## Changes the string to floating, so that I can use numerical operations
     tempCelsius = ((tempFahrenheit - 32)*(5/9)) ## Formula to change F to C
@@ -15,8 +14,7 @@ if (tempMeasurement == "Fahrenheit"): ## If user inputs fahrenheit
 
  ##Code to switch from Celsius to Fahrenheit
 elif(tempMeasurement == "Celsius"): ## If user inputs celsius
-    print("Great! What is the temperature you would like to convert?") ##Gets users to input the number they are looking to convert
-    tempCelsius = input() ## Collects user's answer in the tempCelsius variable
+    tempCelsius = input("Great! What is the temperature you would like to convert? ") ## Collects user's answer in the tempCelsius variable
     print("Wonderful, let me do some quick math to convert " + tempCelsius + "C to Fahrenheit for you.") ## lets the user know what the computer is doing, acts as a confirmation that data was input correctly
     tempCelsius = float(tempCelsius) ##changes string to floating, so that I can use numerical operations
     tempFahrenheit = ((tempCelsius * 9/5) + 32) ##applies the formula to change celsius to fahrenheit
